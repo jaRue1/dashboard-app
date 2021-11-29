9b2617f1c69dc2762fdd9e554d383bb2cbd67846
jaRue1 <jaruejohnson@outlook.com>
2021-11-29 00:10:04 -0500

    completed user list page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import "./app.css"
 import Sidebar from "./components/sidebar/Sidebar.jsx"
 import Topbar from "./components/topbar/Topbar.jsx"
 import Home from "./pages/home/Home"
+import UserList from "./pages/userList/UserList"
+import { BrowserRouter as Router, Route ,Switch } from "react-router-dom"
 function App() {
   return (
-    <div className="app">
+    <Router className="app">
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home/>
+        <Switch>
+          <Route exact path="/users" component={UserList}/>
+        </Switch>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+        </Switch>
       </div>
-    </div>
+    </Router>
   )
 }
 
