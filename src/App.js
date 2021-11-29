c411e980f5f820551af397bdbc7d9267391d677c
jaRue1 <jaruejohnson@outlook.com>
2021-11-28 20:54:17 -0500

    finished featured info component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react"
 import "./app.css"
 import Sidebar from "./components/sidebar/Sidebar.jsx"
 import Topbar from "./components/topbar/Topbar.jsx"
+import Home from "./pages/home/Home"
 function App() {
   return (
     <div className="app">
       <Topbar />
       <div className="container">
         <Sidebar />
-        <div className="others">Other Pages</div>
+        <Home/>
       </div>
     </div>
   )
---
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import "./featuredInfo.css"
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+function FeaturedInfo() {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <span className="featuredTitle">Revenue</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$2,100</span>
+          <span className="featuredMoneyRate">-15.00 <ArrowDownward className="featuredIcon negative"/></span>
+        </div>
+        <span className="featureSub">Compare to last month</span>
+      </div>
+      {/* 2---------- */}
+      <div className="featuredItem">
+        <span className="featuredTitle">Sales</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$4,100</span>
+          <span className="featuredMoneyRate">-1.30 <ArrowDownward className="featuredIcon negative"/></span>
+        </div>
+        <span className="featureSub">Compare to last month</span>
+      </div>
+      {/* 3----------- */}
+      <div className="featuredItem">
+        <span className="featuredTitle">Cost</span>
+        <div className="featuredMoneyContainer">
+          <span className="featuredMoney">$2,100</span>
+          <span className="featuredMoneyRate">+5.00 <ArrowUpward className="featuredIcon"/></span>
+        </div>
+        <span className="featureSub">Compare to last month</span>
+      </div>
+    </div>
+  )
+}
+
+export default FeaturedInfo
