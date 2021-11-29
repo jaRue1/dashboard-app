22b8df4368e0c43a392317feea015c8d478700e8
jaRue1 <jaruejohnson@outlook.com>
2021-11-28 20:18:29 -0500

    finished ui for sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react"
+import "./app.css"
+import Sidebar from "./components/sidebar/Sidebar.jsx"
 import Topbar from "./components/topbar/Topbar.jsx"
 function App() {
-  return <div className="app">
-     <Topbar/>
+  return (
+    <div className="app">
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">Other Pages</div>
+      </div>
     </div>
+  )
 }
 
 export default App
